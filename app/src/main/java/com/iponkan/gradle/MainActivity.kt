433f6b6ebc127d5ee580379ccad3afd5ae57dc1f
iponkan <iponkan@gmail.com>
2019-09-30 11:47:12 +0800

    init

---
+++ app/src/main/java/com/iponkan/gradle/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.iponkan.gradle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
